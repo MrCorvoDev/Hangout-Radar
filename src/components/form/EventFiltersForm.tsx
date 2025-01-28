1492e0009cd8389b4446824b33d20afd6ac6ff98
MrCorvoDev <mr.corvo.dev@gmail.com>
2025-01-28 19:30:24 +0400

    feat: Add EventFilters

---
+++ src/components/form/EventFiltersForm.tsx
@@ -0,0 +1,110 @@
+import {faFilter} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {useDebounce, useMediaQuery} from '@uidotdev/usehooks';
+import {Dispatch, useEffect, useState} from 'react';
+import styled from 'styled-components';
+
+import AccordionProvider from '../../contexts/AccordionContext';
+import {TagProvider} from '../../contexts/TagContext';
+import {layout} from '../../styles/theme';
+import em from '../../styles/utils/em';
+import md from '../../styles/utils/md';
+import {EventFiltersType} from '../../types/global';
+import AccordionButton from '../accordion/AccordionButton';
+import AccordionContent from '../accordion/AccordionContent';
+import EventCountries from './EventCountries';
+import EventDates from './EventDates';
+import EventSegments from './EventSegments';
+import Input from './Input';
+import Label from './Label';
+
+const Form = styled.form`
+   position: sticky;
+   z-index: 4;
+   top: 0;
+   padding: ${em(24)} ${em(16)};
+   background: ${props => props.theme.color2 as string};
+   border-radius: 8px;
+`;
+const FlexTopBar = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   gap: ${em(8)};
+   @media (${md(layout.md3)}) {
+      gap: ${em(24)};
+   }
+`;
+const FilterIcon = styled(AccordionButton)`
+   transition: 0.3s;
+   @media (hover: hover) {
+      &:hover {
+         color: ${props => props.theme.color3 as string};
+      }
+   }
+`;
+const FormBody = styled.div`
+   padding-top: ${em(32)};
+   display: flex;
+   flex-direction: column;
+   gap: ${em(16)};
+`;
+
+interface EventFiltersFormComponentProps extends EventFiltersFormProps {
+   isPC: boolean;
+}
+const EventFiltersFormComponent = ({
+   setFilters,
+   isPC,
+}: EventFiltersFormComponentProps) => {
+   const [searchQuery, setSearchQuery] = useState('');
+   const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
+   useEffect(() => {
+      setFilters(prev => ({...prev, searchQuery: debouncedSearchQuery}));
+   }, [debouncedSearchQuery, setFilters]);
+
+   return (
+      <Form>
+         <FlexTopBar>
+            <EventDates setFilters={setFilters} />
+            {!isPC && (
+               <FilterIcon>
+                  <FontAwesomeIcon icon={faFilter} />
+               </FilterIcon>
+            )}
+         </FlexTopBar>
+         <AccordionContent>
+            <FormBody>
+               <Label title='Event Keyword'>
+                  <Input
+                     name='search'
+                     type='text'
+                     value={searchQuery}
+                     onChange={e => setSearchQuery(e.target.value)}
+                  />
+               </Label>
+               <EventCountries setFilters={setFilters} />
+               <EventSegments setFilters={setFilters} />
+            </FormBody>
+         </AccordionContent>
+      </Form>
+   );
+};
+
+interface EventFiltersFormProps {
+   setFilters: Dispatch<React.SetStateAction<EventFiltersType>>;
+}
+const EventFiltersForm = ({setFilters}: EventFiltersFormProps) => {
+   const isPC = useMediaQuery(`(${md(layout.md3, 'min')})`);
+
+   return (
+      <AccordionProvider defaultOpened={isPC}>
+         <TagProvider>
+            <EventFiltersFormComponent setFilters={setFilters} isPC={isPC} />
+         </TagProvider>
+      </AccordionProvider>
+   );
+};
+
+export default EventFiltersForm;
---
+++ src/components/form/EventSegments.tsx
@@ -0,0 +1,106 @@
+import {Dispatch, useEffect, useState} from 'react';
+import Select, {GroupBase, OptionsOrGroups} from 'react-select';
+import styled from 'styled-components';
+
+import useAppSelector from '../../hooks/useAppSelector';
+import {SegmentType, useGetSegmentsQuery} from '../../store/api/localDataApi';
+import SelectStyles from '../../styles/components/SelectStyles';
+import {EventFiltersType} from '../../types/global';
+import ApiFallback from '../apiFallback';
+import Label from './Label';
+
+const SelectStylesContainer = styled.div`
+   ${SelectStyles}
+   .react-select__multi-value {
+      background: ${props => props.theme.color1 as string};
+   }
+`;
+
+interface OptionType {
+   label: string;
+   value: string;
+}
+const convertSegmentsToOptions = (segments: SegmentType[]) => {
+   const options: OptionType[] = [];
+
+   segments.forEach(segment => {
+      const segmentName = segment.name;
+      const segmentId = segment.id;
+
+      segment.genres.forEach(genre => {
+         options.push({
+            label: `${segmentName} > ${genre.name}`,
+            value: `${segmentId} ${genre.id}`,
+         });
+      });
+   });
+
+   return options;
+};
+const convertOptionsToParams = (
+   options: OptionsOrGroups<OptionType, GroupBase<OptionType>>,
+) => {
+   let segments: string[] = [];
+   let genres: string[] = [];
+
+   (options as OptionType[]).forEach(option => {
+      const [segmentId, genreId] = option.value.split(' ');
+
+      segments.push(segmentId);
+      genres.push(genreId);
+   });
+
+   segments = segments.filter(
+      (value, index) => segments.indexOf(value) === index,
+   );
+   genres = genres.filter((value, index) => genres.indexOf(value) === index);
+
+   return {segments, genres};
+};
+
+interface EventSegmentsProps {
+   setFilters: Dispatch<React.SetStateAction<EventFiltersType>>;
+}
+const EventSegments = ({setFilters}: EventSegmentsProps) => {
+   const userSegments = useAppSelector(state => state.userPreferences.segments);
+   const [selectedOptions, setSelectedOptions] = useState(
+      convertSegmentsToOptions(userSegments),
+   );
+
+   const {data: segments, error, isLoading} = useGetSegmentsQuery();
+   const showFallback = isLoading || error;
+
+   useEffect(() => {
+      const {segments, genres} = convertOptionsToParams(selectedOptions);
+
+      setFilters(prev => ({
+         ...prev,
+         segmentId: segments,
+         genreId: genres,
+      }));
+   }, [segments, setFilters, userSegments, selectedOptions]);
+
+   return (
+      <>
+         {showFallback ? (
+            <ApiFallback isLoading={isLoading} error={error} />
+         ) : (
+            <Label title='Genres'>
+               <SelectStylesContainer>
+                  <Select
+                     isMulti
+                     options={convertSegmentsToOptions(segments!)}
+                     className='react-select'
+                     classNamePrefix='react-select'
+                     value={selectedOptions}
+                     onChange={options => {
+                        setSelectedOptions([...options]);
+                     }}
+                  />
+               </SelectStylesContainer>
+            </Label>
+         )}
+      </>
+   );
+};
+export default EventSegments;
