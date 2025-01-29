0d017340c265da4edad3d8a12249452b1fc21fe4
MrCorvoDev <mr.corvo.dev@gmail.com>
2025-01-29 17:41:00 +0400

    fix(Event): Return Unknown Genre for undefined genres

--- src/utils/parseEvent.tsx
+++ src/utils/parseEvent.tsx
@@ -35,7 +35,7 @@ const getGenres = (classification: ClassificationType) => {
                   return item.name;
                }
 
-               return '';
+               return 'Unknown Genre';
             }),
       ),
    ];
